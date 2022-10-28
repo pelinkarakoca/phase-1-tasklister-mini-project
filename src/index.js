4d9e03426850d09a719c6737d249c949e1697141
Pelin Karakoca <karakocapelin@gmail.com>
2022-10-28 16:43:28 +0300

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  document.querySelector('form').addEventListener('submit', (e) => {
+    e.preventDefault();
+    addToDo(e.target.new_task_description.value)})
 });
+
+function addToDo(todo) {
+  let p = document.createElement('p');
+  let btn = document.createElement('button');
+  btn.textContent = 'X';
+  btn.addEventListener('click', deleteToDo);
+  p.textContent = `${todo} `;
+  p.appendChild(btn);
+  document.querySelector('ul').appendChild(p);
+}
+
+function deleteToDo(e) {
+    e.target.parentNode.remove();
+}
